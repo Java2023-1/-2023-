2e45b25dbce7a02fa434f394f068335660d80b9f
2302528 <146153829+2302528@users.noreply.github.com>
2023-09-27 12:20:28 +0900

    처음올린

---
+++ 자바 과제.js
@@ -0,0 +1,43 @@
+console.log('my' + 'cat');
+console.log('1' + 2);
+console.log('string literals');
+console.log('ellie\'s book');
+
+console.log(1 + 1);
+console.log(1 - 1);
+console.log(1 / 1);
+console.log(1 * 1);
+console.log(5 % 2);
+console.log(2 ** 3);
+
+let counter = 2;
+const preIncrement = ++counter;
+console.log('preIncrement:');
+const postIncrement = counter++;
+console.log('postIncrement:');
+const preDecrement = --counter;
+console.log('preDecrement:');
+const postDecrement = counter--;
+console.log('postDecrement:');
+
+console.log(20 < 6);
+console.log(10 <= 6);
+console.log(10 > 6);
+console.log(10 >= 6);
+
+const value1 = false;
+const value2 = 4 < 2;
+
+console.log('or:');
+console.log('and:');
+console.log(!value1);
+
+const stringFive = '5';
+const numberFive = 5;
+
+console.log(stringFive == numberFive);
+console.log(stringFive != numberFive);
+
+
+console.log(stringFive === numberFive);
+console.log(stringFive !== numberFive);
